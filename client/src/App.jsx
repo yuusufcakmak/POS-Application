535d68020ffbd3ad5ac09e90914b2d3330839184
Yusuf Çakmak <yusuf.cakmak.2000@gmail.com>
2024-12-12 10:27:16 +0300

    Categories Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
+import React from "react";
+import Categories from "./components/categories/Categories";
 import Header from "./components/header/Header";
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      home
-    </div>
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+          <Categories />
+        </div>
+        <div className="products flex-[8]">
+          <div className="">products</div>
+        </div>
+        <div className="">
+          <div>cart totals</div>
+        </div>
+      </div>
+    </>
   );
 }
 
