32e9b912d168b2d607fd8c58495e0c892a25eba1
Yusuf ÇAkmak <91435129+yuusufcakmak@users.noreply.github.com>
2024-12-26 13:44:26 +0300

    add mongoDB,product-category schema model
    Create a MongoDB schema to define and manage product categories, including fields like name and timestamps for organizing products efficiently in the database.

--- api/server.js
+++ api/server.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const port = 5000;
+
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => {
+  connect();
+  console.log(`Example app listening on port ${port}`);
+});
