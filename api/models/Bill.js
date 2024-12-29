96fcccbe1f1e188f9c4fd49719344712c3193c3f
Yusuf Çakmak <yusuf.cakmak.2000@gmail.com>
2024-12-30 02:51:47 +0300

    hotfix

--- api/models/Bill.js
+++ api/models/Bill.js
@@ -5,7 +5,7 @@ const BillSchema = mongoose.Schema(
     customerName: { type: String, require: true },
     customerPhoneNumber: { type: String, require: true },
     paymentMode: { type: String, require: true },
-    cartItem: { type: Array, require: true },
+    cartItems: { type: Array, require: true },
     subTotal: { type: Number, require: true },
     tax: { type: Number, require: true },
     totalAmount: { type: Number, require: true },
