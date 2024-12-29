200074abc7657116f9af408674377f3574c33ef6
Yusuf Çakmak <yusuf.cakmak.2000@gmail.com>
2024-12-30 00:31:44 +0300

    product client-api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage";
 import StatisticPage from "./pages/StatisticPage";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/bills" element={<BillPage />} />
         <Route path="/customers" element={<CustomerPage />} />
         <Route path="/statistic" element={<StatisticPage />} />
+        <Route path="/products" element={<ProductPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react";
+import ProductItem from "./ProductItem";
+import { PlusOutlined, EditOutlined } from "@ant-design/icons";
+import Add from "./Add";
+import { useNavigate } from "react-router-dom";
 
-const Products = () => {
+const Products = ({ categories }) => {
   const [products, setProducts] = useState([]);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getProducts = async () => {
@@ -20,20 +26,28 @@ const Products = () => {
   return (
     <div className="products-wrapper grid grid-cols-card gap-4">
       {products.map((item) => (
-        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-          <div className="product-img">
-            <img
-              src={item.img}
-              alt=""
-              className="h-28 object-cover w-full border-b"
-            />
-          </div>
-          <div className="product-info flex flex-col p-3">
-            <span className="font-bold">{item.title}</span>
-            <span>{item.price}₺</span>
-          </div>
-        </div>
+        <ProductItem item={item} key={item._id} />
       ))}
+
+      <div
+        className="product-item border hover:shadow-lg cursor-pointer transition-all select-none bg-purple-800 flex justify-center items-center hover:opacity-90 min-h-[180px]"
+        onClick={() => setIsAddModalOpen(true)}
+      >
+        <PlusOutlined className="text-white md:text-2xl" />
+      </div>
+      <div
+        className="product-item border hover:shadow-lg cursor-pointer transition-all select-none bg-orange-800 flex justify-center items-center hover:opacity-90 min-h-[180px]"
+        onClick={() => navigate("/products")}
+      >
+        <EditOutlined className="text-white md:text-2xl" />
+      </div>
+      <Add
+        isAddModalOpen={isAddModalOpen}
+        setIsAddModalOpen={setIsAddModalOpen}
+        categories={categories}
+        products={products}
+        setProducts={setProducts}
+      />
     </div>
   );
 };
